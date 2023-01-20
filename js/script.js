fd6c6b29e2454ee96ae8d327cfdbbc2b60e7e752
Abdul Mufid <110360966+eabdalmufid@users.noreply.github.com>
2023-01-20 18:46:41 +0700

    Update script.js

--- js/script.js
+++ js/script.js
@@ -1,11 +1,11 @@
 function play() {
-var audio = new Audio('https://f.top4top.io/m_1911q8utn1.mp3');audio.play();}
+var audio = new Audio('audio/audio.mp3');audio.play();}
 function nonFt() {document.getElementById('foto').style.display = "none";} function showFt() {document.getElementById('foto').style.display = "block";}
   function nonDiv() {document.getElementById('Content').style.display = "none";} function showDiv() {document.getElementById('Content').style.display = "block";}
   function trm() {document.getElementById('trm').style.margin = "0";}
   function whs() {document.getElementById('whs').style.margin = "12px 12px 12px 0";document.getElementById('trm').style.display = "none";}
   function iloveu() {if(u==text2.length){document.getElementById('sp1').style.display = "none";document.getElementById('sp2').style.display = "block";}}
-  function fotoKt() {document.body.style.backgroundColor = "#7B94D1";document.getElementById('ftKm').style.margin = "0 110px 0 0";document.getElementById('ftAk').style.opacity = "1";document.getElementById('ftAk').style.margin = "0 0 0 60px";
+  function fotoKt() {document.getElementById('ftKm').style.margin = "0 110px 0 0";document.getElementById('ftAk').style.opacity = "1";document.getElementById('ftAk').style.margin = "0 0 0 60px";
   var e1 = document.getElementById('ftKm');e1.classList.add("degdeg");var e2 = document.getElementById('ftAk');e2.classList.add("degdeg");}
   
   function ketik() {
